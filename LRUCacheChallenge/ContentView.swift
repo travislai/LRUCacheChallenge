31f9b90df98153c397bd3dffaff4c53e88767259
Travis Lai <travislai@msn.com>
2025-04-29 16:10:22 +0100

    Initial Commit

---
+++ LRUCacheChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LRUCacheChallenge
+//
+//  Created by Travis Lai on 29/4/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LRUCacheChallenge/LRUCacheChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LRUCacheChallengeApp.swift
+//  LRUCacheChallenge
+//
+//  Created by Travis Lai on 29/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct LRUCacheChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
